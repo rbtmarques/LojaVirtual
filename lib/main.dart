89cc1dbc62e7ab578854eef4d20ba460565003a1
Davi Wesley <davi@dimensiva.online>
2019-01-15 11:09:29 -0300

    corrigidas dependencias e configurações do firestore e adicionados widgets de gradiente e barra flutuante

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "Loja Virtual",
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
   }
 }
